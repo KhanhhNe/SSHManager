f3c7a2bec7566370012ef5e882b6b417df29746f
KhanhhNe <khanhluong3005@gmail.com>
2021-06-22 09:42:31 +0700

    Fix build script cause FileNotFoundError when running.

--- build.py
+++ build.py
@@ -15,7 +15,7 @@ while not packages_path.endswith('site-packages'):
 PyInstaller.__main__.run([
     'main.py', '--name=SSHManager', '--icon=logo.ico', '--onedir', '--noconfirm', '--clean',
     '--add-data=templates;templates',
-    '--add-binary=controllers/*.exe;controllers', '--add-binary=logo.ico;.',
+    '--add-binary=controllers/stnlc.exe;.', '--add-binary=logo.ico;.',
     f'--paths={packages_path}',
     '--hidden-import=engineio.async_drivers.threading'
 ])
